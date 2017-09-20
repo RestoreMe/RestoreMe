2f87a3c4929415861406fe7381b5ed0edf6338f5
Soham Roy <soham3.1415@gmail.com>
2017-09-20 01:13:30 -0500

    Created and referenced FilesDestinationsController

---
+++ src/restoreMe/FilesDestinationsController.java
@@ -0,0 +1,20 @@
+package restoreMe;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class FilesDestinationsController implements Initializable {
+
+	public FilesDestinationsController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
