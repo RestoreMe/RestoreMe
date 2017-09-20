3d59ffdd3d59df2509fe08d22298331d75ae93f1
Soham Roy <soham3.1415@gmail.com>
2017-09-20 01:16:30 -0500

    Continued work on DirectoryTree class

--- src/restoreMe/DirectoryTree.java
+++ src/restoreMe/DirectoryTree.java
@@ -1,7 +1,12 @@
 package restoreMe;
 
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 public class DirectoryTree {
 
+	private ArrayList<Path> roots;
+	
 	public DirectoryTree() {
 		// TODO Auto-generated constructor stub
 	}
