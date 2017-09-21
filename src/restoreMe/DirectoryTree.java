a618eb1fc34b8471177dca83a69b4e81d2540f04
Unknown <soham3.1415@gmail.com>
2017-09-20 20:42:18 -0500

    DirectoryTree Update

--- src/restoreMe/DirectoryTree.java
+++ src/restoreMe/DirectoryTree.java
@@ -1,14 +1,45 @@
 package restoreMe;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXTreeView;
+
+import javafx.scene.control.TreeItem;
+import javafx.scene.image.ImageView;
 
 public class DirectoryTree {
 
 	private ArrayList<Path> roots;
+	private JFXTreeView<JFXCheckBox> tree;
 	
-	public DirectoryTree() {
-		// TODO Auto-generated constructor stub
+	public DirectoryTree(ArrayList<Path> roots, JFXTreeView<JFXCheckBox> tree) {
+		this.roots = roots;
+		this.tree = tree;
+		
+		tree.setShowRoot(false);
+		
+		/*checkBox.setIndeterminate(true);
+		
+		Path q = Paths.get("C\\a");
+		ImageView z = new ImageView("/application/Logo.png");
+		z.setPreserveRatio(true);
+		z.setFitWidth(30);
+		TreeItem<Path> root = new TreeItem<Path>(q);
+		System.out.println(root);
+		
+		TreeItem<Path> a = new TreeItem<Path>(q);
+		TreeItem<Path> b = new TreeItem<Path>();
+		a.setGraphic(z);
+		root.getChildren().add(a);
+		root.getChildren().add(b);
+		treeView.setRoot(root);*/
+	}
+	
+	public void addTreeItem() {
+		
 	}
 
 }
--- src/restoreMe/FilesDestinationsController.java
+++ src/restoreMe/FilesDestinationsController.java
@@ -3,10 +3,17 @@ package restoreMe;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXTreeView;
+
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class FilesDestinationsController implements Initializable {
 
+	@FXML
+	JFXTreeView<JFXCheckBox> backupFilesTreeView;
+	
 	public FilesDestinationsController() {
 		// TODO Auto-generated constructor stub
 	}
@@ -14,7 +21,6 @@ public class FilesDestinationsController implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-
 	}
 
 }
