d055a0f124cf450402cd1c6d144e59809bbc1d59
Soham Roy <soham3.1415@gmail.com>
2017-09-26 21:53:42 -0500

    Changed FileTreeItem

--- src/restoreMe/FileTreeItem.java
+++ src/restoreMe/FileTreeItem.java
@@ -4,63 +4,44 @@ import java.nio.file.Path;
 
 import com.jfoenix.controls.JFXCheckBox;
 
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
-public class FileTreeItem extends HBox {
+public class FileTreeItem extends JFXCheckBox {
 	
 	private Path path;
-	private Label label = new Label();
-	private JFXCheckBox checkbox = new JFXCheckBox();
-	private ImageView image;
+	private MaterialDesignIconView icon = new MaterialDesignIconView();
+	private boolean isDirectory;
 	
-	private final static double hBoxSpace = 5;
-	private final static double imageWidth = 30;
-	private final static double imageTextGap = 5;
 	
-	//Constructor for creating this class with only path argument
 	public FileTreeItem(Path path) {
-		super(hBoxSpace);
+		setPath(path);
+		icon.setGlyphSize(20);
+		setGraphic(icon);
+	}
+	
+	private void setIcon() {
+		if(isDirectory)
+			icon.setGlyphName("FOLDER");
+		else
+			icon.setGlyphName("FILE");
+	}
+	
+	public Path getPath() {
+		return path;
+	}
+	
+	public void setPath(Path path) {
 		this.path = path;
-		
-		label.setText(this.path.toString());
-		
-		this.getChildren().addAll(checkbox, label);
-		this.setAlignment(Pos.CENTER_LEFT);
+		isDirectory = this.path.toFile().isDirectory();
+		setIcon();
+		setText(path.getFileName().toString());
 	}
 	
-	//Constructor for creating this class path and imagePath arguments
-	public FileTreeItem(Path path, String imagePath) {
-		super(hBoxSpace);
-		this.path = path;
-		
-		label.setText(this.path.toString());
-		
-		setImage(imagePath);
-		
-		this.getChildren().addAll(checkbox, label);
-		this.setAlignment(Pos.CENTER_LEFT);
-	}
-	
-	//Sets the image
-	public void setImage(String imagePath) {
-		image = new ImageView(imagePath);
-		image.setPreserveRatio(true);
-		image.setFitWidth(imageWidth);
-		
-		label.setGraphic(image);
-		label.setGraphicTextGap(imageTextGap);
-	}
-	
-	//returns the checkbox
-	public JFXCheckBox getCheckbox() {
-		return checkbox;
-	}
-	
-	//returns the text on the label
-	public String toString() {
-		return label.getText();
+	public boolean isDirectory() {
+		return isDirectory;
 	}
 }
